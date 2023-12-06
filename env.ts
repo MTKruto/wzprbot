4acdfe88bb420a6d9dda282f3f671667bae54e00
Roj <ez@roj.im>
2023-12-06 22:18:57 +0300

    Load .env

--- env.ts
+++ env.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/std@0.208.0/dotenv/load.ts";
 import { cleanEnv, num, str } from "https://deno.land/x/envalid@0.1.2/mod.ts";
 
 export default cleanEnv(Deno.env.toObject(), {
